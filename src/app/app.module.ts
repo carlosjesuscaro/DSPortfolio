285307daf6fdfccfa290ecec8f0945a9c3754e0c
Carlos Caro <carlosjesuscaro@icloud.com>
2021-03-14 21:26:31 -0400

    Before routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -9,6 +10,13 @@ import { ProjectsComponent } from './projects/projects.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { FooterComponent } from './footer/footer.component';
+import { CertificationsComponent } from './certifications/certifications.component';
+
+const appRoutes: Routes = [
+  /*{path: 'home', component: AppComponent},*/
+  /*{path: 'projects', component: PPageComponent},*/
+  {path: 'certifications', component: CertificationsComponent}
+];
 
 @NgModule({
   declarations: [
@@ -19,10 +27,12 @@ import { FooterComponent } from './footer/footer.component';
     ProjectsComponent,
     AboutComponent,
     ContactComponent,
-    FooterComponent
+    FooterComponent,
+    CertificationsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
